f9ca1af924fc0ea31f1f732acfc189d609dbf1e9
Ayush Mohanty <imeshan2006@gmail.com>
2026-07-07 17:39:05 +0530

    Initial commit

--- Backend/routes/matchRoutes.js
+++ Backend/routes/matchRoutes.js
@@ -1,35 +1,12 @@
-// ============================================
-// Import Express
-// ============================================
-
 const express = require("express");
-
 const router = express.Router();
 
-
-// ============================================
-// Import Controller
-// ============================================
-
 const {
-
     saveMatch,
     getMatches
-
 } = require("../controllers/matchController");
 
-
-// ============================================
-// Routes
-// ============================================
-
-// Save Match
 router.post("/", saveMatch);
 router.get("/", getMatches);
-
-
-// ============================================
-// Export Router
-// ============================================
 
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,6 @@
-// ============================================
-// Load Environment Variables
-// ============================================
-
 require("dotenv").config();
 
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
-// ============================================
-// Imports
-// ============================================
 
 const express = require("express");
 const cors = require("cors");
@@ -15,30 +8,12 @@ const cors = require("cors");
 const connectDB = require("./config/database");
 const matchRoutes = require("./routes/matchRoutes");
 
-// ============================================
-// App Configuration
-// ============================================
-
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
-// ============================================
-// Connect to MongoDB
-// ============================================
-
 connectDB();
-
-// ============================================
-// Middleware
-// ============================================
 
 app.use(cors());
 app.use(express.json());
-
-// ============================================
-// Routes
-// ============================================
 
 app.use("/api/match", matchRoutes);
 
@@ -47,10 +22,6 @@ app.get("/", (req, res) => {
     res.send("Tic Tac Toe Backend is Running!");
 });
 
-// ============================================
-// Start Server
-// ============================================
-
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
