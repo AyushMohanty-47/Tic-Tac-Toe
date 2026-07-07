ba387b3d653f4e082c227a24c5f23b08a5729b83
Ayush Mohanty <imeshan2006@gmail.com>
2026-07-07 18:54:53 +0530

    Fixed API endpoint for history

--- Frontend/history.js
+++ Frontend/history.js
@@ -3,7 +3,7 @@ const backBtn = document.getElementById("backBtn");
 
 async function loadHistory() {
     try {
-        const response = await fetch("https://tic-tac-toe-t88q.onrender.com/");
+        const response = await fetch("https://tic-tac-toe-t88q.onrender.com/api/match");
         const result = await response.json();
         const matches = result.data;
         historyTable.innerHTML = "";
